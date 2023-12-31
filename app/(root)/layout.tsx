60c67a6f929e5247aeeb6569286169abc2d4fcfa
vishalsolanki-v <vishalthakur2463@gmail.com>
2023-12-31 21:59:11 +0530

    implemented left side bar desktop

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import DesktopLeftbar from "@/components/shared/navbar/DesktopLeftbar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 const Layout = ({children}:{children:React.ReactNode})=>{
@@ -5,7 +6,7 @@ const Layout = ({children}:{children:React.ReactNode})=>{
         <main className="background-light850_dark100 relative">
             <Navbar/>
             <div className="flex">
-                LeftSidebar
+                <DesktopLeftbar/>
                 <section className="mx-md:pb-14 flex min-h-screen flex-1 flex-col px-6 pt-36 sm:px-14">
                     <div className="mx-auto w-full max-w-5xl">
                         {children}

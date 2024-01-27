9b5e2aee4b49241cc983adbf7cd6fcde304c013f
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-01-27 14:00:09 +0530

    lint fixes

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod'
 export const QuestionSchema = z.object({
     title: z.string().min(5, {
-      message: "Title must be at least 5 characters.",
+        message: "Title must be at least 5 characters.",
     }).max(130, {message: "Title cannot be more than 130 characters"}),
     explanation: z.string().min(100),
     tags:z.array(z.string().min(1).max(15)).min(1).max(4),
-  })
+})

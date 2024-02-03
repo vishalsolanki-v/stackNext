f9b70906cb53d261828c3ad9d2d648a813144f0f
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-02-03 12:48:09 +0530

    tags model

---
+++ database/Tags.model.ts
@@ -0,0 +1,20 @@
+import { Schema,models,model,Document } from "mongoose";
+
+export interface Itags extends Document {
+name: string;
+description: string;
+questions?: Schema.Types.ObjectId[];
+followers?: Schema.Types.ObjectId[];
+createdOn: Date;
+}
+
+const TagsSchema = new Schema({
+    name: {type:String,required:true},
+    description: {type:String,required:true},
+    questions: [{type:Schema.Types.ObjectId,ref:'Question'}],
+    followers: [{type:Schema.Types.ObjectId,ref:'User'}],
+    createdOn: [{type:Date,default:Date.now}],
+})
+
+const Tags = models.Tag || model('tag',TagsSchema);
+export default Tags;

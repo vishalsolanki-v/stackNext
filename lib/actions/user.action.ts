b93712740143cf9a4792d288863717cff3df670b
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-02-05 23:56:28 +0530

    added webhooks create user and update user functionality

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,8 +2,34 @@
 
 import User from "@/database/User.model";
 import { connectToDatabase } from "../mongoose"
+import { CreateUserParams, GetUserByIdParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
-export async function getUserById(params:any){
+export async function createUser(userData:CreateUserParams){
+    try {
+        connectToDatabase();
+        const newUser = await User.create(userData);
+        return newUser;
+    } catch (error) {
+        console.info(error);
+        throw error; 
+    }
+}
+export async function updateUser(params:UpdateUserParams){
+    try {
+        connectToDatabase();
+        const {clerkId,updateData,path} = params; 
+        await User.findByIdAndUpdate({clerkId},updateData,{
+            new:true
+        });
+        revalidatePath(path)
+    } catch (error) {
+        console.info(error);
+        throw error; 
+    }
+}
+
+export async function getUserById(params:GetUserByIdParams){
     try {
         connectToDatabase();
         const {userId} = params;

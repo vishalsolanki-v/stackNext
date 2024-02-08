24364d9fc4a5adebce77e457c8420c3d96c8a276
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-02-08 23:19:47 +0530

    login new

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 let isConnected: boolean = false;
 
 export const connectToDatabase = async()=>{
+    
     mongoose.set('strictQuery', true);
     if(!process.env.MONGODB_URL){
         return console.error('MISING MONGOODB URL');

dbac670a49b34e368d930f2e7ffe3f7fa99be872
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-04-08 23:38:03 +0530

    votes ui

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from '@/components/shared/AllAnswers';
 import Metrix from '@/components/shared/Metrix';
 import ParseHTML from '@/components/shared/ParseHTML';
 import RenderTags from '@/components/shared/RenderTags';
+import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action'
 import { getUserById } from '@/lib/actions/user.action';
 import { convertTime, formatNumber } from '@/lib/utils';
@@ -11,68 +12,77 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-const Page = async ({params,searchParams}:any) => {
-console.info(params,searchParams)
-const result = await getQuestionById({questionId:params.id});
-const {userId:clerkId} = auth();
-let mongoUser;
-if(clerkId){
-  mongoUser = await getUserById({userId:clerkId})
-}
+const Page = async ({ params, searchParams }: any) => {
+  console.info(params, searchParams)
+  const result = await getQuestionById({ questionId: params.id });
+  const { userId: clerkId } = auth();
+  let mongoUser;
+  if (clerkId) {
+    mongoUser = await getUserById({ userId: clerkId })
+  }
   return (
     <>
-    <div className='flex-start w-full flex-col'>
-<div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
-  <Link href={`/profile/${result?.author?.clerkId}`}  className='flex items-center justify-start gap-1'>
-    <Image
-    src={result?.author?.picture}
-    width={22}
-    height={22}
-    className='rounded-full w-[22px] h-[22px]'
-    alt='author profile picture'
-    />
-    <p className='paragraph-semibold text-dark300_light700'>{result?.author?.name}</p>
-  </Link>
-  <div className='flex justify-end'>
-VOATING
-  </div>
-</div>
-<p className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>{result?.title} </p>
+      <div className='flex-start w-full flex-col'>
+        <div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+          <Link href={`/profile/${result?.author?.clerkId}`} className='flex items-center justify-start gap-1'>
+            <Image
+              src={result?.author?.picture}
+              width={22}
+              height={22}
+              className='rounded-full w-[22px] h-[22px]'
+              alt='author profile picture'
+            />
+            <p className='paragraph-semibold text-dark300_light700'>{result?.author?.name}</p>
+          </Link>
+          <div className='flex justify-end'>
+            <Votes
+              type="question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result?.upVotes?.length}
+              hasupVoted={result?.upVotes?.includes(mongoUser?._id)}
+              downvotes={result?.downVotes?.length}
+              hasdownVoted={result?.downvotes?.includes(mongoUser?._id)}
+              hasSaved={mongoUser?.saved?.includes(result._id)}
+            />
+          </div>
+        </div>
+        <p className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>{result?.title} </p>
 
-    </div>
-    <div className='mb-8 mt-5 flex flex-wrap gap-4'>
-    <Metrix
-                    imgUrl="/assets/icons/clock.svg"
-                    alt="clock icon"
-                    value={` asked ${convertTime(result?.createdAt)}`}
-                    title=" Votes"
-                    textStyles="small-medium text-dark400_light800"
-                />
-                <Metrix
-                    imgUrl="/assets/icons/message.svg"
-                    alt="message"
-                    value={formatNumber(result?.answer?.length)}
-                    title=" Answers"
-                    textStyles="small-medium text-dark400_light800"
-                />
-                <Metrix
-                    imgUrl="/assets/icons/eye.svg"
-                    alt="eye"
-                    value={formatNumber(result?.views)}
-                    title=" Views"
-                    textStyles="small-medium text-dark400_light800"
-                />
-    </div>
-    <ParseHTML data={result?.content}/>
-    <div className='mt-8 flex flex-wrap gap-2'>
-      {result?.tags?.map((tag:any)=>(
-        <RenderTags _id={tag?._id} showCount={false} key={tag?._id} name={tag?.name} />
-      ))}
-    </div>
+      </div>
+      <div className='mb-8 mt-5 flex flex-wrap gap-4'>
+        <Metrix
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
+          value={` asked ${convertTime(result?.createdAt)}`}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metrix
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatNumber(result?.answer?.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metrix
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatNumber(result?.views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+      <ParseHTML data={result?.content} />
+      <div className='mt-8 flex flex-wrap gap-2'>
+        {result?.tags?.map((tag: any) => (
+          <RenderTags _id={tag?._id} showCount={false} key={tag?._id} name={tag?.name} />
+        ))}
+      </div>
 
-<AllAnswers questionId={result?._id} userId={JSON.stringify(mongoUser?._id)} totalAnswers={result?.answers?.length}/>
+      <AllAnswers questionId={result?._id} userId={JSON.stringify(mongoUser?._id)} totalAnswers={result?.answers?.length} />
 
-    <Answer question={result?.content} questionId={JSON.stringify(result?._id)} authorId={JSON.stringify(mongoUser?._id)}/>
+      <Answer question={result?.content} questionId={JSON.stringify(result?._id)} authorId={JSON.stringify(mongoUser?._id)} />
     </>
   )
 }
---
+++ components/shared/Votes.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { formatNumber } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react'
+interface votesT {
+    type: string;
+    userId: string;
+    itemId: string;
+    hasdownVoted: boolean;
+    hasupVoted: boolean;
+    upvotes: number;
+    downvotes: number;
+    hasSaved?: boolean;
+}
+const Votes = ({ type, userId, itemId, hasdownVoted, hasupVoted, upvotes, downvotes, hasSaved }: votesT) => {
+   const handleSave = ()=>{
+
+   }
+
+   const handleVote = (action:string)=>{
+
+   }
+
+    return (
+        <div className='flex gap-5'>
+            <div className="flex-center gap-2.5">
+                <div className='flex-center gap-1.5'>
+                    <Image src={hasupVoted ? '/assets/icons/upvoted.svg' : '/assets/icons/upvote.svg'}
+                        width={18} height={18} alt='upVote'
+                        className='cursor-pointer'
+                        onClick={() => handleVote('upvote')}
+                    />
+                    <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+                        <p className='subtle-medium text-dark400_light900'>
+                            {formatNumber(upvotes || 0)}
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-center gap-1.5'>
+                    <Image src={hasdownVoted ? '/assets/icons/downvoted.svg' : '/assets/icons/downvote.svg'}
+                        width={18} height={18} alt='upVote'
+                        className='cursor-pointer'
+                        onClick={() => handleVote('downvote')}
+                    />
+                    <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+                        <p className='subtle-medium text-dark400_light900'>
+                            {formatNumber(downvotes || 0)}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <Image src={hasSaved ? '/assets/icons/star-filled.svg' : '/assets/icons/star-red.svg'}
+                        width={18} height={18} alt='star'
+                        className='cursor-pointer'
+                        onClick={handleSave}
+                    />
+        </div>
+    )
+}
+
+export default Votes
